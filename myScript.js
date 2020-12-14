055fa77560d83f632f993116ef068c335f50c0e8
teejay026 <67028361+teejay026@users.noreply.github.com>
2020-12-14 18:17:21 +1100

    update form and calender

--- myScript.js
+++ myScript.js
@@ -1,6 +1,9 @@
-<script id="submitForm"> 
-    submit(
-c
+<script id="alert"> 
+var Name = document.getElementById("name").value;
+document.getElementById("name").value = text;
+var DueDate = document.getElementById("duedate").value;
+document.getElementById("duedate").value = date;
+alert("Hi " + dog + ", you are requesting a commission be completed by " + duedate + "?")
     )
 
 </script>
